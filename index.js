fc42314b0e6dcfc48b0e3083a2d78332a57d7e46
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-07-27 21:24:49 -0400

    Adding robot class

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const SSC = require('./lib/ssc.js');
 const Sequence = require('./lib/sequence.js');
+const Robot = require('./lib/robot.js');
 
 //
 // Exports SSC && Sequence
 //
-module.exports = {SSC, Sequence};
+module.exports = {SSC, Sequence, Robot};
